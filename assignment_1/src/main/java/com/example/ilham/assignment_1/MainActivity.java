c75171418534a7ff208353fd8ca437e27434d00b
Ilham <Ilhaamm99@gmail.com>
2015-03-31 22:54:26 +0200

    Done (:

--- assignment_1/src/main/java/com/example/ilham/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/com/example/ilham/assignment_1/MainActivity.java
@@ -1,17 +1,66 @@
 package com.example.ilham.assignment_1;
 
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+    TextView t; // variabel för textView
 
+    @Override
+    protected void onPostResume() {
+        Log.i("myApp", "RESUMED");
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("myApp", "PAUSED");
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i("myApp", "DESTROYED");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i("myApp", "STOPPED");
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i("myApp", "STARTED");
+        super.onStart();
+    }
+
+    public void buttonOnClicked(View v){ // Metod - vad som ska hända när knappen är "clicked"
+        Log.i("myAPP", "Button pressed");
+        Button b = (Button) findViewById(R.id.nextButton); // Knappen - refererar till specifik knappen
+        t = (TextView) findViewById(R.id.textView); // TextView - ref. till specifik textView
+
+        Resources res = getResources(); // Hänvisar till resources mappen?
+        String[] Quotes = res.getStringArray(R.array.myQuotes); // Min string array
+
+        t.setText(Quotes[new Random().nextInt(Quotes.length)]); // Här säger man till vad som ska visas på TextView.
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
